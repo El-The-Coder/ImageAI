05d70a1d65633104e0ee0e050580fbfd7fe7b26d
/Lmrzx <121160077+El-The-Coder@users.noreply.github.com>
2023-01-25 21:24:15 +0700

    Add files via upload

---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FromField, Loader } from '../components';
+const CreatePost = () => {
+    const navigate = useNavigate();
+    const [form, setform] = useState({
+        name: '',
+        prompt: '',
+        photo: '',
+
+    });
+    const [generatingImg, setGeneratingImg] = useState(false);
+    const [loading, setLoading] = useState(false);
+    return (
+        <section className="max-w-7xl mx-auto" >
+            <div>
+                <h1 className="font-extrabold text-[#222328] text-[32px] ">Create</h1>
+                <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and visually stunning images through by DALL-E AI and share them with the community</p>
+            </div>
+
+
+        </section>
+    )
+}
+
+export default CreatePost
